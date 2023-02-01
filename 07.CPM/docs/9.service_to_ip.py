9c41dd7ea46f9891d36e559aa9f753302e667673
lamtp1 <106371799+lamtp1@users.noreply.github.com>
2023-02-01 10:55:36 +0700

    update service to ip full ip

--- 07.CPM/docs/9.service_to_ip.py
+++ 07.CPM/docs/9.service_to_ip.py
@@ -8,31 +8,33 @@ ip_url = 'http://dcim.viettel.vn/api/service/service-users/'
 
 headers = {
     'Content-Type': "application/json",
-    'Authorization': "Token   babc4e664e88d033892f5f0240bd86664b5c9c48",
+    'Authorization': "Token    7e4ed69be74cfd475a1c361002b15b0169420f08",
     'Cache-Control': "no-cache",
     }
 
 service_file = open("service_code.txt", "r")
 w =  open('service_to_ip.txt', 'w', encoding='UTF-8')
-
 for code in service_file:
-    qr_code = {"name":code}
+    qr_code = {"code":code}
     rp= requests.request("GET", service_url, headers=headers, params=qr_code).json()['results'][0]
     if rp['id'] !=None:
-        service_id = rp['id']
+        service_id = str(rp['id'])
     else:
         service_id = None  # co hien thi gi tren man hinh khong?
 
     if service_id != None:
-        service_id = {"service_id":service_id}
-        rp2 = requests.request("GET", ip_url , headers=headers, params=service_id).json()['results']
-        for id in rp2:
-            i=rp2.index(id) # moi id trong response se duoc danh index bat dau tu 0
-            rp2i = rp2[i]
-            ip = str(rp2i['instance']['name'])
-            print ('Ma dich vu: ' + code.strip() + ' ' + 'co IP la: '+ ip + '\n')
-            s = 'Ma dich vu: ' + code.strip() + ' ' + 'co IP la: '+ ip + '\n'
-            w.write(s)
+        count = requests.request("GET", ip_url , headers=headers, params=service_id).json()['count']
+        offset = 0
+        limit = count
+        while offset < count:
+            qr_param = {"service_id":service_id, "limit":limit, "offset": offset}
+            rp2 = requests.request("GET", ip_url , headers=headers, params=qr_param).json()['results']
+            for id in rp2:
+                ip = str(id['instance']['name'])
+                print ( code.strip() + ' ' +  ip + '\n')
+                s =  code.strip() + ' ' + ip + '\n'
+                w.write(s)
+            offset = offset + count
     else:
         service = None
 
